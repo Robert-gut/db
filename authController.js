8f7efb495eafe6de6c3af7a664c0595873ec25a8
Robert <Robert@step.rivne>
2023-08-21 21:11:27 +0300

    added login

--- authController.js
+++ authController.js
@@ -1,32 +1,69 @@
 const User = require('./modules/User')
 const Role = require('./modules/Role')
-
 const bcrypt = require('bcrypt');
+
+const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { secret } = require('./config')
+
+
+const generateAccessToken = (id, username, roles) =>{
+    const payload = {
+        id,
+        username,
+        roles
+    }
+    return jwt.sign(payload, secret, { expiresIn: '1h' })
+}
 
 class authController {
     async rigistration (req, res){
         try {
+            // valid
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({message: 'Помилка реєстрації.', errors})
+            }
+            // condidate
             const {username, password} = req.body
             const condidate = await User.findOne({username})
             if(condidate){
-                return res.status(400).json({message: 'Користувач з таким іменем уже існує'})
+                return res.status(400).json({message: 'Користувач з таким іменем уже існує.'})
             }
+            // hashPassword
             const hashPassword = bcrypt.hashSync(password, 8);
             const userRole = await Role.findOne({value: 'ADMINISTRATOR'})
+            // save user
             const user = new User({username, password: hashPassword, roles: [userRole.value]})
             await user.save()
-            return res.json({message: 'Користувач успішно зареєстрований'})
+            return res.json({message: 'Користувач успішно зареєстрований.'})
         } catch (error) {
            console.log(error);
-           res.status(400).json({message:'Refistaration error'})
+           res.status(400).json({message:'Registaration error.'})
         }
     }
 
     async login (req, res){
         try {
-            
+            const {username, password} = req.body
+            // find user
+            const user = await User.findOne({username})
+            if(!user){
+                return res.status(400).json({message: `Користувача ${username} не існує.`})
+            }
+            // check password
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if(!validPassword){
+                return res.status(400).json({message: 'Невірний пароль'})
+            }
+            // create jwt
+            const token = generateAccessToken(user._id, user.username, user.roles)
+            console.log(token);
+            return res.json({token})
+
         } catch (error) {
             console.log(error); 
+            return res.status(400).json({message:'Login error.'})
         }
     }
 
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,15 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const {check} = require('express-validator')
 
 
-router.post('/registration', controller.rigistration)
+router.post('/registration',
+[
+    check('username', `Ім'я користувача не може бути порожнім.`).notEmpty(),
+    check('password', 'Пароль може мати мінімум 8 символів і максимум 16 символів.').isLength({min:8, max:16})
+] 
+,controller.rigistration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
