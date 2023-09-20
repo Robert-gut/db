ac063dd574b1bc6f697e1138c95810ac3c05f483
Robert <Robert@step.rivne>
2023-09-20 20:57:13 +0300

    added apiurl

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://3.125.43.47:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }))
